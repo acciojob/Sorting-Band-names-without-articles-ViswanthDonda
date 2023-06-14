fac08a54d46e2bf38093deca797f6daf95668bb5
ViswanthDonda <131549379+ViswanthDonda@users.noreply.github.com>
2023-06-14 21:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
+let bandNames = ['The USA', 'The UAE', 'India', 'The Bahamas'];
 
+// Function to sort band names in lexicographic order excluding articles
+function sortBandNames(bandNames) {
+  const articles = ['a', 'an', 'the'];
+  
+  // Custom sorting function
+  function compare(a, b) {
+    const aName = a.replace(/^(a|an|the)\s+/i, '');
+    const bName = b.replace(/^(a|an|the)\s+/i, '');
+    return aName.localeCompare(bName);
+  }
+  
+  return bandNames.sort(compare);
+}
+
+// Sort the band names
+const sortedBandNames = sortBandNames(bandNames);
+
+// Create the <ul> and add the sorted band names as <li> items
+const ulElement = document.getElementById('band');
+sortedBandNames.forEach(function(bandName) {
+  const liElement = document.createElement('li');
+  liElement.textContent = bandName;
+  ulElement.appendChild(liElement);
+});
